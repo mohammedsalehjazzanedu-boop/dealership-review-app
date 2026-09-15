37534eac1736d439d7147e6daf9f62b7df553943
mohammedsalehjazzanedu-boop <mohammedsaleh.jazzan.edu@gmail.com>
2026-09-14 17:57:48 -0700

    Fix self-referencing urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,20 +1,28 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path
+from . import views
 
-from djangoapp import views as djangoapp_views
+app_name = 'djangoapp'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
+    path('register', views.registration, name='register'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_request, name='logout'),
 
-    # نفس الروابط الرسمية لكن بدون بادئة djangoapp/ (لتطابق التسمية الرسمية بالضبط)
-    path('fetchDealers', djangoapp_views.fetch_dealers, name='fetch_dealers_root'),
-    path('fetchDealers/<str:state>', djangoapp_views.fetch_dealers_by_state, name='fetch_dealers_by_state_root'),
-    path('fetchDealer/<int:dealer_id>', djangoapp_views.fetch_dealer_by_id, name='fetch_dealer_by_id_root'),
-    path('fetchReviews/dealer/<int:dealer_id>', djangoapp_views.fetch_reviews_by_dealer, name='fetch_reviews_root'),
-    path('djangoapp/analyze/<str:text>', djangoapp_views.analyze_review_sentiment, name='analyze_path_root'),
+    # الأسماء الداخلية القديمة (تُستخدم من واجهة React الحالية)
+    path('dealers', views.get_dealerships, name='get_dealerships'),
+    path('dealers/state/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', views.get_dealer_by_id, name='get_dealer_by_id'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='get_dealer_reviews'),
+    path('review/add', views.add_review, name='add_review'),
 
-    # أي رابط تاني (صفحات React) يخدمه ملف index.html
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    path('carmakes', views.get_cars_nested, name='get_cars_nested'),
+    path('get_cars', views.get_cars, name='get_cars'),
+    path('analyze', views.analyze_review_sentiment, name='analyze_review_sentiment'),
+
+    # الأسماء الرسمية المطلوبة بالتقييم
+    path('fetchDealers', views.fetch_dealers, name='fetch_dealers'),
+    path('fetchDealers/<str:state>', views.fetch_dealers_by_state, name='fetch_dealers_by_state'),
+    path('fetchDealer/<int:dealer_id>', views.fetch_dealer_by_id, name='fetch_dealer_by_id'),
+    path('fetchReviews/dealer/<int:dealer_id>', views.fetch_reviews_by_dealer, name='fetch_reviews_by_dealer'),
+    path('analyze/<str:text>', views.analyze_review_sentiment, name='analyze_review_sentiment_path'),
 ]
